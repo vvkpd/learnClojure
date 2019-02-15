2d0c485cac5f175411e0db7139e88667c648e150
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2019-02-15 19:06:42 +0530

    added factorial function

--- sample.clj
+++ sample.clj
@@ -19,3 +19,12 @@
 ; to reverse a list just do 
 (reverse [2 3 4 5 7 90])
 
+; to select only nth elements from a list just do 
+(take-nth 3 [2 3 4 5 7 90])
+
+(defn factorial-of
+  [number]
+  (if (<= number 1) 1
+      (* (factorial-of (dec number)) number)))
+
+(factorial-of 5)
